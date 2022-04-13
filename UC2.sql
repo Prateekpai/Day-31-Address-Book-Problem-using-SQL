1407586c046f5f3b1df1a2bf3cae889683ec3936
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-13 08:39:39 +0530

    Creating Address Book Table

---
+++ UC2.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE AddressBookService;
+
+CREATE TABLE AddressBook
+(
+FirstName Varchar(250),
+LastName Varchar(250),
+Address Varchar(250),
+City Varchar(250),
+State Varchar(250),
+ZIP int,
+PhoneNumber Varchar(250),
+Email Varchar(250)
+)
+
+SELECT * FROM AddressBook;
